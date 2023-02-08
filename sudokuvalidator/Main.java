95aa1d1d3b3a6759c729ff81dcc2c62e4020dca3
Gopika G <71868599+g0pika@users.noreply.github.com>
2023-02-08 09:17:39 +0530

    Update Main.java

--- sudokuvalidator/Main.java
+++ sudokuvalidator/Main.java
@@ -1,17 +1,24 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int[][] board = {
-                {4, 3, 0, 0, 6, 0, 0, 0, 0},
-                {6, 0, 0, 5, 7, 1, 0, 0, 0},
-                {0, 9, 7, 0, 0, 0, 0, 6, 0},
-                {8, 0, 0, 0, 9, 0, 0, 0, 7},
-                {3, 0, 0, 6, 0, 2, 0, 0, 5},
-                {9, 0, 0, 0, 4, 0, 0, 0, 8},
-                {0, 1, 0, 0, 0, 0, 8, 7, 0},
-                {0, 0, 0, 9, 5, 7, 0, 0, 6},
-                {0, 0, 0, 0, 1, 0, 0, 5, 9}
-        };
-        SudokuValidator sv = new SudokuValidator(board);
-        System.out.println(sv.isValidSudoku());
+        Scanner sc = new Scanner(System.in);
+        int[][] board = new int[9][9];
+
+        System.out.println("Enter the elements of the 9x9 sudoku board, separated by spaces:");
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                board[i][j] = sc.nextInt();
+            }
         }
+
+        SudokuValidator sudokuValidator = new SudokuValidator(board);
+        boolean isValid = sudokuValidator.isValidSudoku();
+
+        if (isValid) {
+            System.out.println("The given sudoku board is valid.");
+        } else {
+            System.out.println("The given sudoku board is not valid.");
+        }
+
+    }
     }
